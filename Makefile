5d7c6a42c2da77b241aef0f1458e77282cbc5f02
m-kopka <m.kopka@post.cz>
2024-03-03 21:39:09 +0100

    add: os kernel

--- Makefile
+++ Makefile
@@ -12,10 +12,10 @@ CPU = cortex-m4
 TARGET = build/build
 
 # list folders containing C and Assembly source files here
-SRC_DIRS = . stm32f411-hal/src/ stm32f411-hal/src/* os-kernel/src/ os-kernel/src/* src src/*
+SRC_DIRS = . stm32f411-hal/src/ stm32f411-hal/src/* mini-kernel/src/ mini-kernel/src/* src src/*
 
 # list folders containing header files here
-INC_DIRS = . stm32f411-hal/include/ os-kernel/include/ ./include/
+INC_DIRS = . stm32f411-hal/include/ mini-kernel/include/ ./include/
 
 # linker script path
 LINKER_SCRIPT = stm32f411-hal/stm32f4xx_ls.ld
--- src/main.c
+++ src/main.c
@@ -1,23 +1,58 @@
 #include "common_defs.h"
 
+void led_blink_task(void);
+void heartbeat_task(void);
+
 int main() {
 
     rcc_enable_peripheral_clock(RCC_PERIPH_APB1_PWR);
     write_masked(PWR->CR, 0x3 < 14, 0x3 << 14);                                     // voltage regulator scale 1
     set_bits(FLASH->ACR, FLASH_ACR_PRFTEN | FLASH_ACR_ICEN | FLASH_ACR_DCEN);       // flash prefetch, instruction cache, data cache enable
     write_masked(FLASH->ACR, 3 << 0, 0xf << 0);                                     // flash latency 3 wait states (4 CPU cycles)
-    rcc_enable_hse(25000000);
+    rcc_enable_hse(HSE_OSC_FREQUENCY_HZ);
     rcc_set_bus_prescalers(RCC_SYS_CLOCK_DIV1, RCC_PERIPH_CLOCK_DIV2, RCC_PERIPH_CLOCK_DIV1);
-    rcc_pll_init(96000000, RCC_PLL_SOURCE_HSE);
+    rcc_pll_init(CORE_CLOCK_FREQUENCY_HZ, RCC_PLL_SOURCE_HSE);
     rcc_set_system_clock_source(RCC_SYSTEM_CLOCK_SOURCE_PLL);
 
     rcc_enable_peripheral_clock(RCC_PERIPH_AHB1_GPIOC);
     gpio_set_mode(GPIOC, 14, GPIO_MODE_OUTPUT);
 
+    kernel_init(HLCK_frequency_hz);
+    kernel_create_task(led_blink_task, 1000);
+    kernel_create_task(heartbeat_task, 5000);
+    kernel_start();
+
     while (1) {
 
-        gpio_toggle(GPIOC, 14);
-        for (volatile int i = 0; i < 10000000; i++);
+        NVIC_SystemReset();     // kernel crashed
+    }
+}
+
+void led_blink_task(void) {
+
+    rcc_enable_peripheral_clock(LED_GREEN_GPIO_CLOCK);
+    gpio_write(LED_GREEN_GPIO, HIGH);
+    gpio_set_mode(LED_GREEN_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+
+        gpio_toggle(LED_GREEN_GPIO);
+        kernel_sleep_ms(500);
+    }
+}
+
+void heartbeat_task(void) {
+
+    rcc_enable_peripheral_clock(LED_RED_GPIO_CLOCK);
+    gpio_write(LED_RED_GPIO, HIGH);
+    gpio_set_mode(LED_RED_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1) {
+
+        gpio_write(LED_RED_GPIO, LOW);
+        kernel_sleep_ms(50);
+        gpio_write(LED_RED_GPIO, HIGH);
+        kernel_sleep_ms(1950);
     }
 }
 
