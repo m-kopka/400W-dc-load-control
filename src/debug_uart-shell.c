d5c076ccec127fa0fa2076abce17097613a84bc6
m-kopka <m.kopka@post.cz>
2024-03-21 09:33:21 +0100

    debug shell cleanup

--- src/debug_uart-shell.c
+++ src/debug_uart-shell.c
@@ -10,7 +10,6 @@
 //---- INTERNAL FUNCTIONS ----------------------------------------------------------------------------------------------------------------------------------------
 
 #define SHELL_CMD(command) (strcmp(args[0], (command)) == 0)
-
 #define COMPARE_ARG(argnum, str) (strcmp(args[(argnum)], (str)) == 0)
 
 // prints an error message if the user did't provide enough arguments for the command
@@ -21,7 +20,8 @@
                                     return; \
                                 }
 
-uint8_t shell_parse_args(char *input, char **args, uint8_t argc);
+uint8_t __parse_args(char *input, char **args, uint8_t argc);
+uint32_t __parse_hex_string(char *string);
 
 //---- FUNCTIONS -------------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -49,17 +49,15 @@ void shell_update(char *buffer) {
     while ((buffer[0] <= ' ') && (buffer[0] != '\0')) buffer++;
 
     char *args[5];
-    uint8_t argc = shell_parse_args(buffer, args, 5);
+    uint8_t argc = __parse_args(buffer, args, 5);
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    if (SHELL_CMD("ahoj")) {
-
-        debug_print("zdar jak sviňa\n");
-    }
+    if (SHELL_CMD("ahoj")) debug_print("zdar jak sviňa\n");
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
+    // periodically repeats a command
     else if (SHELL_CMD("repeat")) {
 
         if (argc == 1) debug_repeat_stop();
@@ -82,67 +80,91 @@ void shell_update(char *buffer) {
     // resets the processor
     else if (SHELL_CMD("reboot")) {
 
+        debug_print("restarting..\n");
+        kernel_sleep_ms(200);
         NVIC_SystemReset();
     }
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    // sets the fan speed
-    else if (SHELL_CMD("fan")) {
+    // prints the load status
+    else if (SHELL_CMD("status")) {
+
+        uint16_t status = load_get_status();
+        uint16_t faults = load_get_faults(LOAD_FAULT_ALL);
+        uint16_t fault_mask = load_get_fault_mask();
+
+        debug_print("\nLOAD STATUS\n");
+        debug_print("load is ");
+        debug_print((status & LOAD_STATUS_ENABLED) ? "ENABLED\n" : "DISABLED\n");
+        debug_print("fault state is ");
+        debug_print((status & LOAD_STATUS_FAULT) ? "ACTIVE\n" : "not active\n");
+
+        debug_print("\nCOM fault\t");
+        debug_print((faults & LOAD_FAULT_COMMUNICATION) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_COMMUNICATION) ? "(masked)\n" : "(not masked)\n");
+        debug_print("OTP fault\t");
+        debug_print((faults & LOAD_FAULT_OTP) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_OTP) ? "(masked)\n" : "(not masked)\n");
+        debug_print("OCP fault\t");
+        debug_print((faults & LOAD_FAULT_OCP) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_OCP) ? "(masked)\n" : "(not masked)\n");
+        debug_print("OPP fault\t");
+        debug_print((faults & LOAD_FAULT_OPP) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_OPP) ? "(masked)\n" : "(not masked)\n");
+        debug_print("TEMP_L fault\t");
+        debug_print((faults & LOAD_FAULT_TEMP_SENSOR_L) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_TEMP_SENSOR_L) ? "(masked)\n" : "(not masked)\n");
+        debug_print("TEMP_R fault\t");
+        debug_print((faults & LOAD_FAULT_TEMP_SENSOR_R) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_TEMP_SENSOR_R) ? "(masked)\n" : "(not masked)\n");
+        debug_print("FAN1 fault\t");
+        debug_print((faults & LOAD_FAULT_FAN1) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_FAN1) ? "(masked)\n" : "(not masked)\n");
+        debug_print("FAN2 fault\t");
+        debug_print((faults & LOAD_FAULT_FAN2) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_FAN2) ? "(masked)\n" : "(not masked)\n");
+        debug_print("FUSE_L1 fault\t");
+        debug_print((faults & LOAD_FAULT_FUSE_L1) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_FUSE_L1) ? "(masked)\n" : "(not masked)\n");
+        debug_print("FUSE_L2 fault\t");
+        debug_print((faults & LOAD_FAULT_FUSE_L2) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_FUSE_L2) ? "(masked)\n" : "(not masked)\n");
+        debug_print("FUSE_R1 fault\t");
+        debug_print((faults & LOAD_FAULT_FUSE_R1) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_FUSE_R1) ? "(masked)\n" : "(not masked)\n");
+        debug_print("FUSE_R2 fault\t");
+        debug_print((faults & LOAD_FAULT_FUSE_R2) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_FUSE_R2) ? "(masked)\n" : "(not masked)\n");
+        debug_print("EXTERNAL fault\t");
+        debug_print((faults & LOAD_FAULT_EXTERNAL) ? "ACTIVE\t\t" : "not active\t");
+        debug_print((fault_mask & LOAD_FAULT_EXTERNAL) ? "(masked)\n" : "(not masked)\n");
+    }
+
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // clears all load fault flags
+    else if (SHELL_CMD("clearfaults")) {
+
+        load_clear_fault(LOAD_FAULT_ALL);
+        debug_print("all faults cleared.\n");
+    }
+
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    else if (SHELL_CMD("faultmask")) {
 
         shell_assert_argc(1);
+        int mask = __parse_hex_string(args[1]);
 
-        int speed = atoi(args[1]);
-        if (speed >= 0 && speed < 256) {
+        if (mask >= 0 && mask <= 0xffff) {
 
-            fan_set_pwm_override(speed);
+            load_set_fault_mask((uint16_t)mask);
 
-            debug_print("fan pwm override set to ");
-            debug_print(args[1]);
+            debug_print("fault mask changed to ");
+            debug_print_int_hex(load_get_fault_mask(), 4);
             debug_print(".\n");
-
-        } else debug_print("fan pwm range is <0 - 255>");
-    }
-
-    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-    // returns the fan RPM
-    else if (SHELL_CMD("rpm")) {
-
-        for (int fan = 0; fan < 2; fan++) {
-
-            debug_print((fan == FAN1) ? "fan speeds FAN1: " : ", FAN2: ");
-            debug_print_int(fan_get_rpm(fan));
-            debug_print("rpm");
         }
-
-        debug_print(", pwm: "); 
-        debug_print_int((uint16_t)fan_get_pwm() * 100 / 255);
-        debug_print(" %.\n"); 
-    }
-
-    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-    // returns the power transistor temperatures
-    else if (SHELL_CMD("temp")) {
-
-        for (int sensor = 0; sensor < 2; sensor++) {
-
-            debug_print((sensor == TEMP_L) ? "temperatures L: " : ", R: ");
-
-            uint8_t temperature = temp_sensor_read(sensor);
-            temp_sensor_fault_t fault = temp_sensor_read_faults(sensor);
-
-            if (fault == TEMP_SEN_FAULT_OPEN) debug_print("OPEN");
-            if (fault == TEMP_SEN_FAULT_SHORT) debug_print("SHORT");
-            if (fault == TEMP_SEN_FAULT_NONE) {
-            
-                debug_print_int(temperature);
-                debug_print("'C");
-            }
-        }
-
-        debug_print(".\n"); 
     }
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -188,27 +210,12 @@ void shell_update(char *buffer) {
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    else if (SHELL_CMD("status")) {
-
-        uint16_t status = load_get_status();
-        uint16_t faults = load_get_faults(LOAD_FAULT_ALL);
-        uint16_t fault_mask = load_get_fault_mask();
-
-        debug_print("load is ");
-        debug_print((status & LOAD_STATUS_ENABLED) ? "ENABLED.\n" : "DISABLED.\n");
-
-        debug_print("fault state is ");
-        debug_print((status & LOAD_STATUS_FAULT) ? "ACTIVE.\n" : "NOT ACTIVE.\n");
-    }
-
-    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
     // sets the load CC level
     else if (SHELL_CMD("iset")) {
 
         shell_assert_argc(1);
-
         int current = atoi(args[1]);
+
         if (current >= LOAD_MIN_CURRENT_MA && current <= LOAD_MAX_CURRENT_MA) {
 
             load_set_cc_level((uint16_t)current);
@@ -216,6 +223,14 @@ void shell_update(char *buffer) {
             debug_print("CC level set to ");
             debug_print_int(current);
             debug_print(" mA.\n");
+
+        } else {
+
+            debug_print("(!) iset range is <");
+            debug_print_int(LOAD_MIN_CURRENT_MA);
+            debug_print(" - ");
+            debug_print_int(LOAD_MAX_CURRENT_MA);
+            debug_print(">.\n");
         }
     }
 
@@ -241,6 +256,22 @@ void shell_update(char *buffer) {
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
+    // prints all internal current sink currents
+    else if (SHELL_CMD("isen-int")) {
+
+        debug_print("i(L1): ");
+        debug_print_int_dec(internal_isen_read(CURRENT_L1), 2);
+        debug_print(" A\ni(L2): ");
+        debug_print_int_dec(internal_isen_read(CURRENT_L2), 2);
+        debug_print(" A\ni(R1): ");
+        debug_print_int_dec(internal_isen_read(CURRENT_R1), 2);
+        debug_print(" A\ni(R2): ");
+        debug_print_int_dec(internal_isen_read(CURRENT_R2), 2);
+        debug_print(" A\n");
+    }
+
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
     // prints total load current
     else if (SHELL_CMD("psen")) {
 
@@ -251,25 +282,10 @@ void shell_update(char *buffer) {
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    // prints all internal current sink currents
-    else if (SHELL_CMD("isen-int")) {
-
-        debug_print("i(L1): ");
-        debug_print_int_dec(internal_isen_read(CURRENT_L1), 3);
-        debug_print(" A\ni(L2): ");
-        debug_print_int_dec(internal_isen_read(CURRENT_L2), 3);
-        debug_print(" A\ni(R1): ");
-        debug_print_int_dec(internal_isen_read(CURRENT_R1), 3);
-        debug_print(" A\ni(R2): ");
-        debug_print_int_dec(internal_isen_read(CURRENT_R2), 3);
-        debug_print(" A\n");
-    }
-
-    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
+    // selects the voltage sense source
     else if (SHELL_CMD("vsensrc")) {
 
-        if (argc == 0) {
+        if (argc == 1) {
 
             debug_print("voltage sense source is ");
             debug_print((vi_sense_get_vsen_source() == VSEN_SRC_INTERNAL) ? "internal" : "remote");
@@ -287,9 +303,8 @@ void shell_update(char *buffer) {
                 vi_sense_set_vsen_source(VSEN_SRC_REMOTE);
                 debug_print("voltage sense source set to remote\n");
 
-            }
+            } else debug_print("(!) invalid argument. Use \"internal\" or \"remote\".\n");
         }
-    
     }
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -298,51 +313,105 @@ void shell_update(char *buffer) {
     else if (SHELL_CMD("vdis")) {
 
         shell_assert_argc(1);
-
         int voltage = atoi(args[1]);
+
         if (voltage >= 0 && voltage <= 100000) {
 
             load_set_discharge_voltage((uint32_t)voltage);
 
-            debug_print("discharge voltage set to: ");
-            debug_print_int(voltage);
-            debug_print(" mV.\n");
-        }
-    }
+            if (voltage > 0) {
 
-    else if (SHELL_CMD("clear")) {
+                debug_print("discharge voltage set to: ");
+                debug_print_int(voltage);
+                debug_print(" mV.\n");
 
-        debug_print("all faults cleared.\n");
-        load_clear_fault(LOAD_FAULT_ALL);
-    }
-
-    else if (SHELL_CMD("mask")) {
-
-        shell_assert_argc(1);
-
-        int mask = atoi(args[1]);
-        if (mask >= 0 && mask <= 0xffff) {
-
-            debug_print("fault mask changed.\n");
-            load_set_fault_mask((uint16_t)mask);
+            } else debug_print("loat auto-disable feature disabled.\n");    
         }
     }
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-    // prints all internal current sink currents
+    // returns the power transistor temperatures
+    else if (SHELL_CMD("temp")) {
+
+        for (int sensor = TEMP_L; sensor <= TEMP_R; sensor++) {
+
+            debug_print((sensor == TEMP_L) ? "temperatures L: " : ", R: ");
+
+            uint8_t temperature = temp_sensor_read(sensor);
+            temp_sensor_fault_t fault = temp_sensor_read_faults(sensor);
+
+            if (fault == TEMP_SEN_FAULT_OPEN) debug_print("OPEN");
+            if (fault == TEMP_SEN_FAULT_SHORT) debug_print("SHORT");
+            if (fault == TEMP_SEN_FAULT_NONE) {
+            
+                debug_print_int(temperature);
+                debug_print("'C");
+            }
+        }
+
+        debug_print(".\n"); 
+    }
+
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // sets the fan speed
+    else if (SHELL_CMD("fan")) {
+
+        shell_assert_argc(1);
+
+        int speed = atoi(args[1]);
+        if (speed >= 0 && speed < 256) {
+
+            fan_set_pwm_override(speed);
+
+            debug_print("fan pwm override set to ");
+            debug_print(args[1]);
+            debug_print(".\n");
+
+        } else debug_print("fan pwm range is <0 - 255>");
+    }
+
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // returns the fan RPM
+    else if (SHELL_CMD("rpm")) {
+
+        for (int fan = 0; fan < 2; fan++) {
+
+            debug_print((fan == FAN1) ? "fan speeds FAN1: " : ", FAN2: ");
+            debug_print_int(fan_get_rpm(fan));
+            debug_print("rpm");
+        }
+
+        debug_print(", pwm: "); 
+        debug_print_int((uint16_t)fan_get_pwm() * 100 / 255);
+        debug_print(" %.\n"); 
+    }
+
+    //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    // prints available commands
     else if (SHELL_CMD("help")) {
 
-        debug_print("list of available commands:\n");
+        debug_print("\nlist of available commands:\n");
+        debug_print("repeat <period_ms> <command> - periodically repeat a command\n");
         debug_print("reboot - restart the system\n");
-        debug_print("fan <speed> - set the fan speed\n");
-        debug_print("rpm - read the fan speed\n");
-        debug_print("en <1 or 0> - enables or disables the load. Returns the load enable state if the command is used without an argument\n");
-        debug_print("iset <current_mA> - set the load CC level\n");
+        debug_print("status - print the load status\n");
+        debug_print("clearfaults - clear all load fault flags\n");
+        debug_print("faultmask <0000 - ffff> - set a fault mask\n");
+        debug_print("en <1 or 0> - enables or disables the load\n");
+        debug_print("iset <current_ma> - set the load CC level\n");
         debug_print("vsen - read the load input voltage\n");
+        kernel_sleep_ms(50);
         debug_print("isen - read the total load current\n");
         debug_print("isen-int - read the individual current sink currents\n");
         debug_print("psen - read the load power\n");
+        debug_print("vsensrc <internal or remote> - set the voltage sense source\n");
+        debug_print("vdis <voltage_mv> - disable the load automatically when the source voltage drops bellow a threshold\n");
+        debug_print("temp - read the power transistor temperatures\n");
+        debug_print("fan <0 - 255> - set the fan pwm\n");
+        debug_print("rpm - read the fan speed\n");
     }
 
     //- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
@@ -354,7 +423,7 @@ void shell_update(char *buffer) {
 
 // separates an input string into a command and arguments all separated by spaces; arguments with spaces are possible by using quotation marks
 // caution: the original string is modified by the function; the function inserts null-terminators after each argument and returns pointers to the arguments in the original string
-uint8_t shell_parse_args(char *input, char **args, uint8_t argc) {
+uint8_t __parse_args(char *input, char **args, uint8_t argc) {
 
     uint8_t  current_arg = 0;                       // index of current argument
     bool     parsing = false;                       // currently parsing an argument, not looking for a beginning of a new argument
@@ -393,6 +462,23 @@ uint8_t shell_parse_args(char *input, char **args, uint8_t argc) {
     *input = '\0';
 
     return (current_arg);
+}
+
+//- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// converts a string containing a hexadecimal number to an unsigned integer
+uint32_t __parse_hex_string(char *string) {
+
+    uint32_t result = 0;
+
+    for (int i = 0; string[i] != '\0'; ++i) {
+        
+        if      (string[i] >= '0' && string[i] <= '9') result = result * 16 + string[i] - '0';
+        else if (string[i] >= 'a' && string[i] <= 'f') result = result * 16 + 10 + string[i] - 'a';
+        else if (string[i] >= 'A' && string[i] <= 'F') result = result * 16 + 10 + string[i] - 'A';
+    }
+
+    return result;
 }
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------
